976815d639b740f235343530604cfd466253abd1
GERLAN PEREIRA MAGALHAES <111335734+gerlan06@users.noreply.github.com>
2023-03-01 04:00:26 -0300

    comit
    primeiro comit

---
+++ script.js
@@ -0,0 +1,30 @@
+window. sr = ScrollReveal ({reset: true})
+
+sr.reveal('.capa', {duration:1500});
+
+sr.reveal('.caixa h3', {
+    rotate:{x:0, y:80, z:0}, duration:2000 });
+
+    sr.reveal('.caixa-container', {
+        rotate:{x:0, y:80, z:0}, duration:2000 });
+    
+
+    sr.reveal('.caixa-container img', {
+        rotate:{x:0, y:80, z:0}, duration:3000 });
+    
+
+    sr.reveal('.caixa p', {
+        rotate:{x:0, y:80, z:0}, duration:2500 });
+
+sr.reveal('.luta-single-container', {duration:1800});
+
+sr.reveal('.luta-single-container2', {duration:1800});
+
+sr.reveal('.luta-single-container3', {duration:1800});
+
+
+sr.reveal('.luta-single-container h3', {
+    rotate:{x:0, y:80, z:0}, duration:2000 });
+
+    sr.reveal('.luta-single-container p', {
+        rotate:{x:0, y:80, z:0}, duration:2500 });
